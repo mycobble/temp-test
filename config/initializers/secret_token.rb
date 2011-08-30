a8c226f5fe1849153111b3b1d5e97f74660433c8
mycobble <mycobble@gmail.com>
2011-08-30 18:19:20 -0500

    Initial master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TempTest::Application.config.secret_token = '32a34d5101c71c84dd3443909dfa5cbc0aa095995610da08e8b186e150deab9865e9d780f13081e6595ef0e5b50a6768cd5777a4fca5d867791e137b16e52122'
